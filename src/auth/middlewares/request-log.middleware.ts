bee62d153b5c9d158fd6c874e7e10ef3466000a3
SimonSchulz <zenkov241091@gmail.com>
2025-07-19 13:02:51 +0200

    change order of log api requests

--- src/auth/middlewares/request-log.middleware.ts
+++ src/auth/middlewares/request-log.middleware.ts
@@ -13,8 +13,6 @@ export const requestLogMiddleware = async (
     const now = new Date();
     const tenSecondsAgo = new Date(now.getTime() - 10 * 1000);
 
-    await requestLogsCollection.insertOne({ ip, url, date: now });
-
     const count = await requestLogsCollection.countDocuments({
       ip,
       url,
@@ -22,7 +20,9 @@ export const requestLogMiddleware = async (
     });
     if (count > 5) {
       res.sendStatus(HttpStatus.TooManyRequests);
+      return;
     } else {
+      await requestLogsCollection.insertOne({ ip, url, date: now });
       next();
     }
   } catch (error) {
