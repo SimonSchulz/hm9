feb03a8ada15328773b2c4f8be30188a91db78d2
SimonSchulz <zenkov241091@gmail.com>
2025-07-19 23:25:50 +0200

    change api logic to db version10

--- src/auth/domain/nodemailer.service.ts
+++ src/auth/domain/nodemailer.service.ts
@@ -1,26 +1,24 @@
 import nodemailer from "nodemailer";
 import { SETTINGS } from "../../core/setting/setting";
-
+let transporter = nodemailer.createTransport({
+  //service: 'gmail',
+  host: "smtp.yandex.ru",
+  port: 465,
+  secure: true,
+  auth: {
+    user: SETTINGS.EMAIL,
+    pass: SETTINGS.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 export const nodemailerService = {
   async sendEmail(
     email: string,
     code: string,
     template: (code: string) => string,
   ): Promise<void> {
-    let transporter = nodemailer.createTransport({
-      //service: 'gmail',
-      host: "smtp.yandex.ru",
-      port: 465,
-      secure: true,
-      auth: {
-        user: SETTINGS.EMAIL,
-        pass: SETTINGS.EMAIL_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-
     await transporter.sendMail({
       from: `"Blogs platform" <${SETTINGS.EMAIL}>`,
       to: email,
