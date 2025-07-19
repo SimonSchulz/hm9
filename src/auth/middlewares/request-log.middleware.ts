0d25e2481fd9aa79aa731cb240dcf6b76dcb780d
SimonSchulz <zenkov241091@gmail.com>
2025-07-19 23:15:06 +0200

    change api logic to db version10

--- src/auth/middlewares/request-log.middleware.ts
+++ src/auth/middlewares/request-log.middleware.ts
@@ -12,20 +12,17 @@ export const requestLogMiddleware = async (
     const url = req.originalUrl;
     const now = new Date();
     const tenSecondsAgo = new Date(now.getTime() - 10 * 1000);
-
+    await requestLogsCollection.insertOne({ ip, url, date: now });
     const count = await requestLogsCollection.countDocuments({
       ip,
       url,
       date: { $gte: tenSecondsAgo },
     });
-    if (count >= 5) {
+    if (count > 5) {
       res.sendStatus(HttpStatus.TooManyRequests);
-      await requestLogsCollection.deleteMany({ url });
       return;
-    } else {
-      await requestLogsCollection.insertOne({ ip, url, date: now });
-      next();
     }
+    next();
   } catch (error) {
     next(error);
   }
